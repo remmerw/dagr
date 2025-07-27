3078cf8c4cfd5ce77583d488cce7a40f51155700
remmerw <remmer.wilts@gmail.com>
2025-07-27 13:57:28 +0200

    Version 0.1.5 ------------- Beautifying

--- dagr/src/commonMain/kotlin/io/github/remmerw/dagr/Pipe.kt
+++ dagr/src/commonMain/kotlin/io/github/remmerw/dagr/Pipe.kt
@@ -16,39 +16,15 @@
 package io.github.remmerw.dagr
 
 import kotlinx.io.Buffer
-import java.io.Closeable
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
 import java.util.concurrent.locks.Condition
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
-import kotlin.time.Duration
-import kotlin.time.DurationUnit
-import kotlin.time.toTimeUnit
 
-/**
- * A source and a sink that are attached. The sink's output is the source's input. Typically each
- * is accessed by its own thread: a producer thread writes data to the sink and a consumer thread
- * reads data from the source.
- *
- * This class uses a buffer to decouple source and sink. This buffer has a user-specified maximum
- * size. When a producer thread outruns its consumer the buffer fills up and eventually writes to
- * the sink will block until the consumer has caught up. Symmetrically, if a consumer outruns its
- * producer reads block until there is data to be read. Limits on the amount of time spent waiting
- * for the other party can be configured with [timeouts][Timeout] on the source and the
- * sink.
- *
- * When the sink is closed, source reads will continue to complete normally until the buffer has
- * been exhausted. At that point reads will return -1, indicating the end of the stream. But if the
- * source is closed first, writes to the sink will immediately fail with an [Exception].
- *
- * A pipe may be canceled to immediately fail writes to the sink and reads from the source.
- */
 internal class Pipe() {
     private val buffer = Buffer()
     private var canceled = false
-    private var sinkClosed = false
-    private var sourceClosed = false
 
     private val lock: ReentrantLock = ReentrantLock()
     private val condition: Condition = lock.newCondition()
@@ -71,10 +47,8 @@ internal class Pipe() {
 
         override fun write(bytes: ByteArray) {
             lock.withLock {
-                check(!sinkClosed) { "closed" }
-                if (canceled) throw InterruptedException("canceled")
 
-                if (sourceClosed) throw Exception("source is closed")
+                if (canceled) throw InterruptedException("canceled")
 
                 buffer.write(bytes, 0, bytes.size)
                 condition.signalAll() // Notify the source that it can resume reading.
@@ -84,30 +58,16 @@ internal class Pipe() {
 
         override fun flush() {
             lock.withLock {
-                check(!sinkClosed) { "closed" }
+
                 if (canceled) throw InterruptedException("canceled")
 
 
-                if (sourceClosed && buffer.size > 0L) {
-                    throw Exception("source is closed")
-                }
-
-                if(buffer.size > 0L){
+                if (buffer.size > 0L) {
                     condition.signalAll() // Notify the source that it can resume reading.
                 }
             }
         }
 
-        override fun close() {
-            lock.withLock {
-                if (sinkClosed) return
-
-
-                if (sourceClosed && buffer.size > 0L) throw Exception("source is closed")
-                sinkClosed = true
-                condition.signalAll() // Notify the source that no more bytes are coming.
-            }
-        }
 
         override fun timeout(): Timeout = timeout
     }
@@ -117,11 +77,10 @@ internal class Pipe() {
 
         override fun read(sink: Buffer, byteCount: Long): Long {
             lock.withLock {
-                check(!sourceClosed) { "closed" }
+
                 if (canceled) throw InterruptedException("canceled")
 
                 while (buffer.size == 0L) {
-                    if (sinkClosed) return -1L
                     timeout.awaitSignal(condition) // Wait until the sink fills the buffer.
                     if (canceled) throw InterruptedException("canceled")
                 }
@@ -132,29 +91,10 @@ internal class Pipe() {
             }
         }
 
-        override fun close() {
-            lock.withLock {
-                sourceClosed = true
-                condition.signalAll() // Notify the sink that no more bytes are desired.
-            }
-        }
-
         override fun timeout(): Timeout = timeout
     }
 
 
-    /**
-     * Fail any in-flight and future operations. After canceling:
-     *
-     *  * Any attempt to write or flush [sink] will fail immediately with an [Exception].
-     *  * Any attempt to read [source] will fail immediately with an [Exception].
-     *
-     * Closing the source and the sink will complete normally even after a pipe has been canceled. If
-     * this sink has been folded, closing it will close the folded sink. This operation may block.
-     *
-     * This operation may be called by any thread at any time. It is safe to call concurrently while
-     * operating on the source or the sink.
-     */
     fun cancel() {
         lock.withLock {
             canceled = true
@@ -164,7 +104,7 @@ internal class Pipe() {
     }
 }
 
-interface Sink : Closeable {
+interface Sink {
     /** Removes `byteCount` bytes from `source` and appends them to this.  */
     fun write(bytes: ByteArray)
 
@@ -174,50 +114,9 @@ interface Sink : Closeable {
     /** Returns the timeout for this sink.  */
     fun timeout(): Timeout
 
-    /**
-     * Pushes all buffered bytes to their final destination and releases the resources held by this
-     * sink. It is an error to write a closed sink. It is safe to close a sink more than once.
-     */
-    override fun close()
 }
 
-
-/**
- * Supplies a stream of bytes. Use this interface to read data from wherever it's located: from the
- * network, storage, or a buffer in memory. Sources may be layered to transform supplied data, such
- * as to decompress, decrypt, or remove protocol framing.
- *
- * Most applications shouldn't operate on a source directly, but rather on a BufferedSource which
- * is both more efficient and more convenient. Use buffer to wrap any source with a buffer.
- *
- * Sources are easy to test: just use a Buffer in your tests, and fill it with the data your
- * application is to read.
- *
- * ### Comparison with InputStream
-
- * This interface is functionally equivalent to java.io.InputStream.
- *
- * `InputStream` requires multiple layers when consumed data is heterogeneous: a `DataInputStream`
- * for primitive values, a `BufferedInputStream` for buffering, and `InputStreamReader` for strings.
- * This library uses `BufferedSource` for all of the above.
- *
- * Source avoids the impossible-to-implement [available()] java.io.InputStream.available method.
- * Instead callers specify how many bytes they [require] BufferedSource.require.
- *
- * Source omits the unsafe-to-compose [mark and reset][java.io.InputStream.mark] state that's
- * tracked by `InputStream`; instead, callers just buffer what they need.
- *
- * When implementing a source, you don't need to worry about the [read()][java.io.InputStream.read]
- * method that is awkward to implement efficiently and returns one of 257 possible values.
- *
- * And source has a stronger `skip` method: BufferedSource.skip won't return prematurely.
- *
- * ### Interop with InputStream
- *
- * Use source to adapt an `InputStream` to a source. Use BufferedSource.inputStream to adapt a
- * source to an `InputStream`.
- */
-interface Source : Closeable {
+interface Source {
     /**
      * Removes at least 1, and up to `byteCount` bytes from this and appends them to `sink`. Returns
      * the number of bytes read, or -1 if this source is exhausted.
@@ -226,12 +125,6 @@ interface Source : Closeable {
 
     /** Returns the timeout for this source.  */
     fun timeout(): Timeout
-
-    /**
-     * Closes this source and releases the resources held by this source. It is an error to read a
-     * closed source. It is safe to close a source more than once.
-     */
-    override fun close()
 }
 
 
@@ -309,22 +202,6 @@ open class Timeout {
         return this
     }
 
-    /**
-     * Throws an [Exception] if the deadline has been reached or if the current thread
-     * has been interrupted. This method doesn't detect timeouts; that should be implemented to
-     * asynchronously abort an in-progress operation.
-     */
-
-    open fun throwIfReached() {
-        if (Thread.currentThread().isInterrupted) {
-            // If the current thread has been interrupted.
-            throw Exception("interrupted")
-        }
-
-        if (hasDeadline && deadlineNanoTime - System.nanoTime() <= 0) {
-            throw Exception("deadline reached")
-        }
-    }
 
     /**
      * Prevent all current applications of this timeout from firing. Use this when a time-limited
@@ -425,147 +302,4 @@ open class Timeout {
         }
     }
 
-    /**
-     * Waits on `monitor` until it is notified. Throws [Exception] if either the thread
-     * is interrupted or if this timeout elapses before `monitor` is notified. The caller must be
-     * synchronized on `monitor`.
-     *
-     * Here's a sample class that uses `waitUntilNotified()` to await a specific state. Note that the
-     * call is made within a loop to avoid unnecessary waiting and to mitigate spurious notifications.
-     *
-     * ```java
-     * class Dice {
-     *   Random random = new Random();
-     *   int latestTotal;
-     *
-     *   public synchronized void roll() {
-     *     latestTotal = 2 + random.nextInt(6) + random.nextInt(6);
-     *     System.out.println("Rolled " + latestTotal);
-     *     notifyAll();
-     *   }
-     *
-     *   public void rollAtFixedRate(int period, TimeUnit timeUnit) {
-     *     Executors.newScheduledThreadPool(0).scheduleAtFixedRate(new Runnable() {
-     *       public void run() {
-     *         roll();
-     *       }
-     *     }, 0, period, timeUnit);
-     *   }
-     *
-     *   public synchronized void awaitTotal(Timeout timeout, int total)
-     *       throws InterruptedIOException {
-     *     while (latestTotal != total) {
-     *       timeout.waitUntilNotified(this);
-     *     }
-     *   }
-     * }
-     * ```
-     */
-    open fun waitUntilNotified(monitor: Any) {
-        try {
-            val hasDeadline = hasDeadline()
-            val timeoutNanos = timeoutNanos()
-
-            if (!hasDeadline && timeoutNanos == 0L) {
-                (monitor as Object).wait() // There is no timeout: wait forever.
-                return
-            }
-
-            // Compute how long we'll wait.
-            val start = System.nanoTime()
-            val waitNanos = if (hasDeadline && timeoutNanos != 0L) {
-                val deadlineNanos = deadlineNanoTime() - start
-                minOf(timeoutNanos, deadlineNanos)
-            } else if (hasDeadline) {
-                deadlineNanoTime() - start
-            } else {
-                timeoutNanos
-            }
-
-            if (waitNanos <= 0) throw TimeoutException("timeout")
-
-            val cancelMarkBefore = cancelMark
-
-            // Attempt to wait that long. This will return early if the monitor is notified.
-            val waitMillis = waitNanos / 1000000L
-            (monitor as Object).wait(waitMillis, (waitNanos - waitMillis * 1000000L).toInt())
-            val elapsedNanos = System.nanoTime() - start
-
-            // If there's time remaining, we probably got the call we were waiting for.
-            if (elapsedNanos < waitNanos) return
-
-            // Return without throwing if this timeout was canceled while we were waiting. Note that this
-            // return is a 'spurious wakeup' because Object.notify() was not called.
-            if (cancelMark !== cancelMarkBefore) return
-
-            throw TimeoutException("timeout")
-        } catch (timeoutException: TimeoutException) {
-            Thread.currentThread().interrupt() // Retain interrupted status.
-            throw timeoutException
-        }
-    }
-
-    /**
-     * Applies the minimum intersection between this timeout and `other`, run `block`, then finally
-     * rollback this timeout's values.
-     */
-    inline fun <T> intersectWith(other: Timeout, block: () -> T): T {
-        val originalTimeout = this.timeoutNanos()
-        this.timeout(
-            minTimeout(
-                other.timeoutNanos(),
-                this.timeoutNanos()
-            ), TimeUnit.NANOSECONDS
-        )
-
-        if (this.hasDeadline()) {
-            val originalDeadline = this.deadlineNanoTime()
-            if (other.hasDeadline()) {
-                this.deadlineNanoTime(
-                    kotlin.math.min(
-                        this.deadlineNanoTime(),
-                        other.deadlineNanoTime()
-                    )
-                )
-            }
-            try {
-                return block()
-            } finally {
-                this.timeout(originalTimeout, TimeUnit.NANOSECONDS)
-                if (other.hasDeadline()) {
-                    this.deadlineNanoTime(originalDeadline)
-                }
-            }
-        } else {
-            if (other.hasDeadline()) {
-                this.deadlineNanoTime(other.deadlineNanoTime())
-            }
-            try {
-                return block()
-            } finally {
-                this.timeout(originalTimeout, TimeUnit.NANOSECONDS)
-                if (other.hasDeadline()) {
-                    this.clearDeadline()
-                }
-            }
-        }
-    }
-
-    companion object {
-
-        fun Timeout.timeout(timeout: Long, unit: DurationUnit): Timeout {
-            return timeout(timeout, unit.toTimeUnit())
-        }
-
-        fun Timeout.timeout(duration: Duration): Timeout {
-            return timeout(duration.inWholeNanoseconds, TimeUnit.NANOSECONDS)
-        }
-
-        fun minTimeout(aNanos: Long, bNanos: Long) = when {
-            aNanos == 0L -> bNanos
-            bNanos == 0L -> aNanos
-            aNanos < bNanos -> aNanos
-            else -> bNanos
-        }
-    }
 }
