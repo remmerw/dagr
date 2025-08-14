9800534d63c85b70fabe122f882b3e6e2fab686d
remmerw <remmer.wilts@gmail.com>
2025-08-14 12:42:21 +0200

    Version 0.3.6 ------------- Optimizations

--- dagr/src/commonMain/kotlin/io/github/remmerw/dagr/ClientConnection.kt
+++ dagr/src/commonMain/kotlin/io/github/remmerw/dagr/ClientConnection.kt
@@ -3,9 +3,9 @@ package io.github.remmerw.dagr
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import kotlinx.io.RawSink
-import kotlinx.io.asSink
 import kotlinx.io.asSource
 import kotlinx.io.buffered
+import java.io.DataOutputStream
 import java.net.Socket
 import kotlin.concurrent.atomics.AtomicBoolean
 import kotlin.concurrent.atomics.ExperimentalAtomicApi
@@ -15,7 +15,7 @@ open class ClientConnection(
 ) : Connection {
 
     private val receiveChannel = socket.inputStream.asSource().buffered()
-    private val sendChannel = socket.outputStream.asSink().buffered()
+    private val sendChannel = DataOutputStream(socket.outputStream)
 
     @OptIn(ExperimentalAtomicApi::class)
     private val closed = AtomicBoolean(false)
@@ -51,6 +51,7 @@ open class ClientConnection(
                 sendChannel.writeLong(request)
                 sendChannel.flush()
                 val count = receiveChannel.readInt()
+                require(count > 0){ "Invalid response $count bytes returned" }
                 receiveChannel.readTo(sink, count.toLong())
                 return count
             } catch (throwable: Throwable) {
