3dbc25c71474e0f0e9dcf6fccdd150b71f243293
remmerw <remmer.wilts@gmail.com>
2025-08-17 14:38:55 +0200

    Version 0.3.8 ------------- Enhance API (offset)

--- dagr/src/commonMain/kotlin/io/github/remmerw/dagr/Listener.kt
+++ dagr/src/commonMain/kotlin/io/github/remmerw/dagr/Listener.kt
@@ -1,10 +1,10 @@
 package io.github.remmerw.dagr
 
-import kotlinx.io.Source
+import kotlinx.io.RawSource
 
 interface Connection : AutoCloseable
 
-data class Data(val source: Source, val length: Long)
+data class Data(val source: RawSource, val length: Long)
 
 interface Acceptor {
     suspend fun request(request: Long, offset: Long): Data
